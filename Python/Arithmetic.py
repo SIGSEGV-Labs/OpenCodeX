4c211b878064e621d7c240bd4cb766d96b07d25d
Nakka Silvan Mohan <silven667@gmail.com>
2026-08-07 23:40:36 +0530

    Add basic arithmetic operations in Arithmetic.py

---
+++ Python/Arithmetic.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print("Addition of the numbers: ", a + b)
+print("Subtraction of the numbers: ", b - a)
+print("Multiplication of the numbers: ", a * b)
+print("Division of the numbers: ", a / b)
+print("Integer division of the numbers: ", b // a)
+print("Remainder of the two numbers: ", a % b)
+print(f"Exponentiation of the numbers: {a ** a}")
